851ce2723b2b6149787d7d7068d69e571e6b8d91
Adrien Cransac <adrien.cransac@gmail.com>
2021-02-11 10:44:54 +0100

    Plot: * Added function to plot timeline

--- src/plot.js
+++ src/plot.js
@@ -15,6 +15,7 @@ const { DateTime } = require('luxon');
  * @param {Date} endDate - The date to which the index is plotted. Can only be sooner than the latest assets value
  * @param {number} maxColumns - The maximum width of the chart in printable columns (number of characters). It is at least 15
  * @param {number} rows - The height of the chart in printable rows (number of lines)
+ * @return {string}
  */
 function plotIndex(assetsValues, index, beginDate, endDate, maxColumns, rows) {
   const selectIndexValues = xIncrementInDays => (selection, currentDate, currentIndexValue, assetsValues, index) => {
@@ -45,7 +46,75 @@ function plotIndex(assetsValues, index, beginDate, endDate, maxColumns, rows) {
 
 // # Timeline
 
+/*
+ * Plot the timeline of a time interval with a specified maximum length. Only passing months and years are plotted
+ * @param {Date} beginDate - The date from which the timeline is plotted
+ * @param {Date} endDate - The date to which the timeline is plotted
+ * @param {number} maxColumns - The maximum length of the timeline in printable columns (number of characters)
+ * @return {string}
+ */
 function plotTimeline(beginDate, endDate, maxColumns) {
+  const timeIncrement = timeStep(beginDate, endDate, maxColumns);
+
+  const plotMonth = (date, availableWidth) => {
+    if (availableWidth >= 3) {
+      return `${DateTime.fromJSDate(date).monthShort.padEnd(availableWidth - 3, " ")}`;
+    }
+    else {
+      return `${DateTime.fromJSDate(date).monthShort[0].padEnd(availableWidth - 1, " ")}`;
+    }
+  };
+
+  const plotYear = (date, availableWidth) => {
+    if (availableWidth > 4) {
+      return `${DateTime.fromJSDate(date).year.toString().padEnd(availableWidth - 4, " ")}`;
+    }
+    else (availableWidth > 2) {
+      return `${DateTime.fromJSDate(date).year.toString().slice(-2).padEnd(availableWidth - 1, " ")}`;
+    }
+    else {
+      return "";
+    }
+  };
+
+  const plotTimelineImpl =
+    (arrowLine, monthLine, yearLine, previousMonth, previousYear, currentDate, monthPlottableWidth, yearPlottableWidth) => {
+      if (currentDate > endDate) {
+        return `${arrowLine}>\n${monthLine}\n${yearLine}`;
+      }
+      else if (previousMonth === currentDate.getMonth()) {
+        return plotTimelineImpl(arrowLine,
+                                monthLine,
+                                yearLine,
+                                previousMonth,
+                                previousYear,
+                                DateTime.fromJSDate(currentDate).plus({days: timeIncrement}).toJSDate(),
+                                monthPlottableWidth + 1,
+                                yearPlottableWidth + 1);
+      }
+      else if (previousYear === currentDate.getFullYear()) {
+        return plotTimelineImpl(`${arrowLine}${new Array(monthPlottableWidth).fill("─").join("")}`,
+                                `${monthLine}${plotMonth(new Date(previousYear, previousMonth), monthPlottableWidth)}`,
+                                yearLine,
+                                currentDate.getMonth(),
+                                previousYear,
+                                DateTime.fromJSDate(currentDate).plus({days: timeIncrement}).toJSDate(),
+                                0,
+                                yearPlottableWidth + 1);
+      }
+      else {
+        return plotTimelineImpl(`${arrowLine}${new Array(monthPlottableWidth).fill("─").join("")}`,
+                                `${monthLine}${plotMonth(new Date(previousYear, previousMonth), monthPlottableWidth)}`,
+                                `${yearLine}${plotYear(new Date(previousYear, previousMonth), yearPlottableWidth)}`,
+                                currentDate.getMonth(),
+                                currentDate.getFullYear(),
+                                DateTime.fromJSDate(currentDate).plus({days: timeIncrement}).toJSDate(),
+                                0,
+                                0);
+      }
+  };
+
+  return plotTimelineImpl("", "", "", beginDate.getMonth(), beginDate.getFullYear(), beginDate, 0, 0);
 }
 
 // # Helpers
@@ -58,5 +127,6 @@ function timeStep(beginDate, endDate, maxSteps) {
 }
 
 module.exports = {
-  plotIndex
+  plotIndex,
+  plotTimeline
 };
