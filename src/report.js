d4ac2933050302abdcf33a00849c1ac536c73c1e
Adrien Cransac <adrien.cransac@gmail.com>
2021-02-22 19:45:19 +0100

    Report: * Added dealing with report interval not being specified by the user * Added test for unspecified interval

--- src/report.js
+++ src/report.js
@@ -1,6 +1,7 @@
 // Copyright (c) Adrien Cransac
 // License: No license
 
+const { valueDate } = require('./assetsvalue.js');
 const { assetsValues, fundName } = require('./fund.js');
 const { index } = require('./index.js');
 const { indexHighlights, indexHistory } = require('./log.js');
@@ -20,28 +21,36 @@ function reportOnFunds(funds, beginDate, endDate, plotWidth, plotHeight) {
     ["Relative Market Value", index]
   ];
 
-  return funds.map(fund => [
-    `# ${fundName(fund)}`,
-    ...indices.map(([indexName, indexFunction]) => {
-      const index = indexFunction(assetsValues(fund));
+  return funds.map(fund => {
+    const valuations = assetsValues(fund);
 
-      return [
-        `## ${indexName}`,
-        "",
-        plotIndex(assetsValues(fund), index, beginDate, endDate, plotWidth, plotHeight),
-        plotTimeline(beginDate, endDate, plotWidth, 13),
-        "",
-        "### Highlights",
-        "",
-        indexHighlights(assetsValues(fund), index, beginDate, endDate),
-        "",
-        "### History",
-        "",
-        indexHistory(assetsValues(fund), index, beginDate, endDate),
-        ""
-      ].join("\n");
-    })
-  ].join("\n")).join("\n");
+    const begin = beginDate ? beginDate : valueDate(valuations[0]);
+
+    const end = endDate ? endDate : valueDate(valuations[valuations.length - 1]);
+
+    return [
+      `# ${fundName(fund)}`,
+      ...indices.map(([indexName, indexFunction]) => {
+        const index = indexFunction(valuations);
+
+        return [
+          `## ${indexName}`,
+          "",
+          plotIndex(valuations, index, begin, end, plotWidth, plotHeight),
+          plotTimeline(begin, end, plotWidth, 13),
+          "",
+          "### Highlights",
+          "",
+          indexHighlights(valuations, index, begin, end),
+          "",
+          "### History",
+          "",
+          indexHistory(valuations, index, begin, end),
+          ""
+        ].join("\n");
+      })
+    ].join("\n");
+  }).join("\n");
 }
 
 module.exports = {
--- test/test_report.js
+++ test/test_report.js
@@ -5,29 +5,10 @@ const { readFundsFromJson } = require('../src/fund.js');
 const { reportOnFunds } = require('../src/report.js');
 const Test = require('@acransac/tester');
 
-function test_reportOnFunds(finish, check) {
-  const testFundsJson = JSON.stringify([
-    {
-      fundName: "Test Fund 1",
-      currency: "USD",
-      assetsValues: [
-        {valueDate: new Date(2021, 0, 24), marketValue: 1000.0, addedCash: 0.0},
-        {valueDate: new Date(2021, 0, 25), marketValue: 2000.0, addedCash: 0.0},
-        {valueDate: new Date(2021, 0, 26), marketValue: 1500.0, addedCash: 0.0}
-      ]
-    },
-    {
-      fundName: "Test Fund 2",
-      currency: "EUR",
-      assetsValues: [
-        {valueDate: new Date(2021, 0, 24), marketValue: 1000.0, addedCash: 0.0},
-        {valueDate: new Date(2021, 0, 25), marketValue: 2000.0, addedCash: 1000.0},
-        {valueDate: new Date(2021, 0, 26), marketValue: 1500.0, addedCash: 0.0}
-      ]
-    }
-  ]);
+// # Helpers
 
-  const control = [
+function control() {
+  return [
     "# Test Fund 1",
     "## Relative Market Value",
     "",
@@ -109,11 +90,44 @@ function test_reportOnFunds(finish, check) {
     "| Jan 26, 2021 | 75.00       |",
     ""
   ].join("\n");
+}
 
+function testFunds() {
+  return JSON.stringify([
+    {
+      fundName: "Test Fund 1",
+      currency: "USD",
+      assetsValues: [
+        {valueDate: new Date(2021, 0, 24), marketValue: 1000.0, addedCash: 0.0},
+        {valueDate: new Date(2021, 0, 25), marketValue: 2000.0, addedCash: 0.0},
+        {valueDate: new Date(2021, 0, 26), marketValue: 1500.0, addedCash: 0.0}
+      ]
+    },
+    {
+      fundName: "Test Fund 2",
+      currency: "EUR",
+      assetsValues: [
+        {valueDate: new Date(2021, 0, 24), marketValue: 1000.0, addedCash: 0.0},
+        {valueDate: new Date(2021, 0, 25), marketValue: 2000.0, addedCash: 1000.0},
+        {valueDate: new Date(2021, 0, 26), marketValue: 1500.0, addedCash: 0.0}
+      ]
+    }
+  ]);
+}
+
+// # Tests
+
+function test_reportOnFunds(finish, check) {
   return finish(check(
-    reportOnFunds(readFundsFromJson(testFundsJson), new Date(2021, 0, 24), new Date(2021, 0, 26), 120, 15) === control));
+    reportOnFunds(readFundsFromJson(testFunds()), new Date(2021, 0, 24), new Date(2021, 0, 26), 120, 15) === control()));
+}
+
+function test_reportOnFundsUnspecifiedInterval(finish, check) {
+  return finish(check(
+    reportOnFunds(readFundsFromJson(testFunds()), undefined, undefined, 120, 15) === control()));
 }
 
 Test.run([
-  Test.makeTest(test_reportOnFunds, "Report On Funds")
+  Test.makeTest(test_reportOnFunds, "Report On Funds"),
+  Test.makeTest(test_reportOnFundsUnspecifiedInterval, "Report On Funds With Unspecified Interval")
 ], "Test Report");
