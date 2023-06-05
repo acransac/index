c08a90834d1310db90e6875ff2df613fcece234b
Adrien Cransac <adrien.cransac@gmail.com>
2023-06-05 21:40:19 +0200

    Account for time value of cash flows in return on investment index

--- src/index.js
+++ src/index.js
@@ -29,34 +29,53 @@ function relativeMarketValue(assetsValues) {
 /*
  * Compute the return on investment for each in a time series of a fund's valuations
  * @param {[AssetsValue]} assetsValues - The assets' values
+ * @param {CompoundCurve} compoundCurve - The time-variable compound operator to compute the time
+ *   value of cash flows
  * @return {[number]}
  */
-function returnOnInvestment(assetsValues) {
-  const roi = (returned, invested, endDate) => {
-    return (duration => returned.dividedBy(invested)
-                                .toPower(new Decimal(1).dividedBy(Math.max(1, duration)))
-                                .minus(1)
-                                .times(100))
-             (DateTime.fromJSDate(endDate).diff(DateTime.fromJSDate(valueDate(assetsValues[0])), "days").as("years"));
-  };
+function returnOnInvestment(assetsValues, compoundCurve) {
+  if (assetsValues.length > 0) {
+    const cashFlow = (assetsValue, isReturned) => [
+      valueDate(assetsValue),
+      isReturned ? - Math.min(0, addedCash(assetsValue)) : Math.max(0, addedCash(assetsValue))
+    ];
 
-  return assetsValues
-    .reduce(([returned, invested, index], assetsValue, id) => {
-      if (id === 0) {
-        return [
-          returned.minus(Math.min(0, addedCash(assetsValue))),
-          invested.plus(Math.max(0, addedCash(assetsValue))),
-          [new Decimal(0)]
-        ];
-      }
-      else {
-        return (roi => [returned.minus(Math.min(0, addedCash(assetsValue))),
-                        invested.plus(Math.max(0, addedCash(assetsValue))),
-                        [...index, roi]])
-                 (roi(returned.plus(valueBeforeAddedCash(assetsValue)), invested, valueDate(assetsValue)));
-      }
-    }, [new Decimal(0), new Decimal(0), []])[2]
-    .map(indexValue => indexValue.toNumber());
+    const roi = (returned, invested, endDate) => {
+      const endValue = cashFlows => {
+        const isoDate = date => DateTime.fromJSDate(date).toISODate();
+
+        return cashFlows.reduce((sum, [cashFlowDate, cashFlowValue]) =>
+          sum + (cashFlowValue > 0.0
+            ? compoundCurve.slice(isoDate(cashFlowDate), isoDate(endDate)).compound(cashFlowValue)
+            : 0.0),
+          0.0);
+      };
+
+      return (duration => new Decimal(endValue(returned))
+        .dividedBy(new Decimal(endValue(invested)))
+        .toPower(new Decimal(1).dividedBy(Math.max(1, duration)))
+        .minus(1)
+        .times(100)
+        .toNumber())
+        (DateTime.fromJSDate(endDate)
+          .diff(DateTime.fromJSDate(valueDate(assetsValues[0])), "days")
+          .as("years"));
+    };
+
+    return assetsValues.slice(1).reduce(([returned, invested, index], assetsValue) =>
+      (roi => [
+        [...returned, cashFlow(assetsValue, true)],
+        [...invested, cashFlow(assetsValue, false)],
+        [...index, roi]
+      ])(roi(
+        [...returned, [valueDate(assetsValue), valueBeforeAddedCash(assetsValue)]],
+        [...invested, [valueDate(assetsValue), 0.0]],
+        valueDate(assetsValue))),
+      [[cashFlow(assetsValues[0], true)], [cashFlow(assetsValues[0], false)], [0.0]])[2];
+  }
+  else {
+    return [];
+  }
 }
 
 module.exports = {
--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@
 // License: MIT
 
 const { makeAssetsValue } = require('../src/assetsvalue.js');
+const { CompoundCurve } = require('../src/compoundcurve.js');
 const { assetsValues, readFundsFromJson } = require('../src/fund.js');
 const { relativeMarketValue, returnOnInvestment } = require('../src/index.js');
 
@@ -53,23 +54,28 @@ test("Index From Fund Json", () => {
 // # Return On Investment
 
 test("Return On Investment Without Assets Value", () => {
-  expect(returnOnInvestment([])).toEqual([]);
+  expect(returnOnInvestment([], new CompoundCurve([["2020-01-01/2024-01-01", 1.00]]))).toEqual([]);
 });
 
 test("Return On Investment With One Assets Value", () => {
-  expect(returnOnInvestment([makeAssetsValue(new Date(2020, 0, 1), 1000.0, 1000.0)])).toEqual([0]);
+  expect(returnOnInvestment(
+    [makeAssetsValue(new Date(2020, 0, 1), 1000.0, 1000.0)],
+    new CompoundCurve([["2020-01-01/2024-01-01", 1.00]])))
+    .toEqual([0]);
 });
 
 test("Return On Investment Example", () => {
   const control = [0, 1, 1, 0.67, 0];
 
-  const test = returnOnInvestment([
-    makeAssetsValue(new Date(2020, 0, 1), 500.0, 500.0),
-    makeAssetsValue(new Date(2020, 6, 1), 1005.0, 500.0),
-    makeAssetsValue(new Date(2021, 0, 1), 1010.0, 0.0),
-    makeAssetsValue(new Date(2021, 6, 1), 1000.0, -10.0),
-    makeAssetsValue(new Date(2022, 0, 1), 990.0, 0.0)
-  ]);
+  const test = returnOnInvestment(
+    [
+      makeAssetsValue(new Date(2020, 0, 1), 500.0, 500.0),
+      makeAssetsValue(new Date(2020, 6, 1), 1005.0, 500.0),
+      makeAssetsValue(new Date(2021, 0, 1), 1010.0, 0.0),
+      makeAssetsValue(new Date(2021, 6, 1), 1000.0, -10.0),
+      makeAssetsValue(new Date(2022, 0, 1), 990.0, 0.0)
+    ],
+    new CompoundCurve([["2020-01-01/2024-01-01", 1.00]]));
 
   expect(test).toBeInstanceOf(Array);
 
